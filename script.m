8c0605bce91e3c5548bd250ed9d43f85c584405e
Catarina Silva <c.alexandracorreia@ua.pt>
2020-03-25 11:11:34 +0000

    Update script

--- script.m
+++ script.m
@@ -1,30 +1,25 @@
-
-ap1 = [250, 100];
-ap2 = [150,100; 350, 100];
-ap3 = [50, 50; 250, 100; 450, 150];
-
-N = [20, 40, 60, 80];
-S = [3, 6];
-W = [40, 60, 80];
+N = [20 40 60 80];
+S = [3 6];
+W = [40 60 80];
 dlt = 1;
 T = 7200;
 pl = 0;
 
-for i=0 : 2
-  for w =1: size(W,1)
-    for s = 1: size(S,1)
-      for n = 1: size(N,1)
-        if i == 0
-         res(N,S,W) = simulatorFunction(N(n),S(s),W(w),dlt,T,ap1,pl)
-        endif
-        if i ==1
-          res(N,S,W) = simulatorFunction(N(n),S(s),W(w),dlt,T,ap2,pl)
-        endif
-        if i == 2
-          res(N,S,W) = simulatorFunction(N(n),S(s),W(w),dlt,T,ap3,pl)
-        endif
-      endfor
-    endfor
-  endfor
-endfor
-save 'results' 'res';
+y=0;
+rs=[];
+for w = 1:size(W,2)
+    for s = 1:size(S,2)
+        for n = 1:size(N,2)
+            avgA = [];
+            avgM = [];
+            for i = 1:10
+                y=y+1
+                [A, M] = simulatorFunction(N(n),S(s),W(w),dlt,T,[150,100; 350,100],pl);
+                avgA = [avgA mean(A)]
+                avgM = [avgM min(M)]
+            end
+            rs(n,s,w,1) = mean(avgA);
+            rs(n,s,w,2) = mean(avgM);
+        end
+    end
+end
